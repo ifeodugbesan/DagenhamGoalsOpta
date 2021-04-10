57a2bd9434ac32934eebff401dc8931636da9298
Ife Odugbesan <ifeodugbesan@yahoo.com>
2021-04-10 23:00:09 +0100

    height fix

--- app/javascript/controllers/common_controller.js
+++ app/javascript/controllers/common_controller.js
@@ -5,11 +5,7 @@ export default class extends Controller {
   connect() {
     this.containerTargets.forEach(container => container.style.minHeight = `${window.innerHeight - 66}px`)
     if (this.hasHomepageTarget) {
-      if (navigator.standalone) {
-        this.homepageTarget.style.minHeight = `${window.innerHeight - 80}px`
-      } else {
-        this.homepageTarget.style.minHeight = `${window.innerHeight - 66}px`
-      }
+      this.homepageTarget.style.minHeight = `${window.innerHeight - 66}px`
     }
   }
 }
