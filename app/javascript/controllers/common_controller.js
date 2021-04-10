7829c29eb3f94dbe1c1992c047bbd496d7258b14
Ife Odugbesan <ifeodugbesan@yahoo.com>
2021-04-10 22:28:02 +0100

    changed hommepage

--- app/javascript/controllers/common_controller.js
+++ app/javascript/controllers/common_controller.js
@@ -1,8 +1,11 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ['container']
+  static targets = ['container', 'homepage']
   connect() {
     this.containerTargets.forEach(container => container.style.minHeight = `${window.innerHeight - 66}px`)
+    if (this.hasHomepageTarget) {
+      this.homepageTarget.style.minHeight = `${window.innerHeight - 66}px`
+    }
   }
 }
