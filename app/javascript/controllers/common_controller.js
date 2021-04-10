2e232fb9314da012fef522ab9276a4332eaef93d
Ife Odugbesan <ifeodugbesan@yahoo.com>
2021-04-10 22:49:01 +0100

    changed homepage height

--- app/javascript/controllers/common_controller.js
+++ app/javascript/controllers/common_controller.js
@@ -5,7 +5,11 @@ export default class extends Controller {
   connect() {
     this.containerTargets.forEach(container => container.style.minHeight = `${window.innerHeight - 66}px`)
     if (this.hasHomepageTarget) {
-      this.homepageTarget.style.minHeight = `${window.innerHeight - 66}px`
+      if (navigator.standalone) {
+        this.homepageTarget.style.minHeight = `${window.innerHeight - 80}px`
+      } else {
+        this.homepageTarget.style.minHeight = `${window.innerHeight - 66}px`
+      }
     }
   }
 }
