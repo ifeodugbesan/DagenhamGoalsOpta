32930cee353058e48defcbb61eadde2304d9adac
Ife Odugbesan <ifeodugbesan@yahoo.com>
2023-07-27 19:40:21 +0100

    updates

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,5 @@
 class Match < ApplicationRecord
-  has_many :match_goals
+  has_many :match_goals, dependent: :destroy
   has_many :goals, through: :match_goals, dependent: :destroy
   has_many :assists, through: :match_goals, dependent: :destroy
   has_many :teams, dependent: :destroy
