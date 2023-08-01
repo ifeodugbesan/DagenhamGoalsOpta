ccf52acdbe5ec26577d533496cb36bd72e3d4a4e
Ife Odugbesan <ifeodugbesan@yahoo.com>
2023-08-01 16:14:21 +0100

    updates

--- app/javascript/controllers/match_goal_controller.js
+++ app/javascript/controllers/match_goal_controller.js
@@ -9,22 +9,24 @@ export default class extends Controller {
     }
 
     const checkFields = () => {
-      let myArray = []
-      let button = this.buttonTarget
-      this.goalTargets.forEach(input => input.checked ? myArray.push(input.checked) : '' )
-      this.assistTargets.forEach(input => input.checked ? myArray.push(input.checked) : '' )
-      // console.log(myArray)
-      this.goalTargets.forEach((input) => {
-        if (myArray.length < 1) {
-          input.checked ? input.nextElementSibling.classList.remove('disabled') : input.nextElementSibling.classList.add('disabled')
+      if (this.hasButtonTarget) {
+        let myArray = []
+        let button = this.buttonTarget
+        this.goalTargets.forEach(input => input.checked ? myArray.push(input.checked) : '' )
+        this.assistTargets.forEach(input => input.checked ? myArray.push(input.checked) : '' )
+        // console.log(myArray)
+        this.goalTargets.forEach((input) => {
+          if (myArray.length < 1) {
+            input.checked ? input.nextElementSibling.classList.remove('disabled') : input.nextElementSibling.classList.add('disabled')
+          } else {
+            input.nextElementSibling.classList.remove('disabled');
+          }
+        });
+        if (myArray.every(isfilled) && myArray.length == 2) {
+          button.classList.remove('disabled')
         } else {
-          input.nextElementSibling.classList.remove('disabled');
+          button.classList.add('disabled')
         }
-      });
-      if (myArray.every(isfilled) && myArray.length == 2) {
-        button.classList.remove('disabled')
-      } else {
-        button.classList.add('disabled')
       }
     }
 
@@ -42,13 +44,13 @@ export default class extends Controller {
 
   }
 
-
-
   toggleGoalActive() {
     this.goalTargets.forEach(input => input.nextElementSibling.classList.remove('match-goal-active'))
     event.currentTarget.classList.toggle('match-goal-active')
-    this.goalContainerTarget.classList.add('d-none')
-    this.assistContainerTarget.classList.remove('d-none')
+    if (this.hasGoalContainerTarget) {
+      this.goalContainerTarget.classList.add('d-none')
+      this.assistContainerTarget.classList.remove('d-none')
+    }
   }
 
   toggleAssistActive() {
